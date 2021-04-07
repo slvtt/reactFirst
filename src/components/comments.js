c2e5dc7fe32e720819a068eed323d5c3eb7e920c
salavat876 <mavlikhanov2002@mail.ru>
2021-04-07 14:50:30 +0400

    Update comments.js

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Comments = ({userName,comment,removeComments},)=>{
+
     const hour = comment.fullDate.hour;
     const minute = comment.fullDate.minute;
+
     const declOfNum = (number, words) => {
-    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+        
+        return words[(number % 100 > 4 && number % 100 < 20) ? 
+            2 : 
+                [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
     }
+    
     return(
         <div key ={comment.id} className ="item-comment">
             <div className="flex-row">
