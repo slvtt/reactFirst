48dc11df45dc333f5a6d556e31e3fbe5d0612182
salavat876 <mavlikhanov2002@mail.ru>
2021-04-08 13:07:55 +0400

    f

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import Comments from './components/comments'
 
 function App(){
     const [comments,setComments] = useState([]);
-    const [userName,setName] = useState([]);
 
     const addComments = (userInput,userInputName) =>{
         if(userInput){
@@ -14,21 +13,28 @@ function App(){
             const hour = NewDate.getHours();
             const minute = NewDate.getMinutes();
             const date = NewDate.getDate();
+
             const newItem = {
                 id:Math.random().toString(36).substr(2,9),
                 comment:userInput,
                 name:userInputName,
                 fullDate:{
-                    hour:hour,
-                    minute:minute,
+                    hour:hour + declOfNum(hour,[' час ', ' часа ', ' часов ']),
+                    minute:minute + declOfNum(minute,[' минута ', ' минуты ', ' минут ']),
                     date:date
                 }
             }
 
             setComments([...comments,newItem])
-            setName([...userName,newItem])
         }
 
+    }
+
+    const declOfNum = (number, words) => {
+
+        return words[(number % 100 > 4 && number % 100 < 20) ? 
+            2 : 
+                [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
     }
 
     const removeComments = () =>{
@@ -40,25 +46,26 @@ function App(){
 
     }
 
-
-
-
     return(
         <div className="fixed-container">
             <Info />
             <InputForm addComments={addComments} />
             <h1>Comments {comments.length}</h1>
+            <ul>
             {comments.map((comment,userName) => {
                 return(
-                    <Comments
-                        userName={userName}
-                        comment={comment}
-                        key={comment.id}
-                        toggleComment={handleComment}
-                        removeComments={removeComments}
-                    />
+                        <li key={comment.id}>
+                        <Comments
+                            userName={userName}
+                            comment={comment}
+                            key={comment.id}
+                            toggleComment={handleComment}
+                            removeComments={removeComments}
+                        />
+                        </li>
                 )
             })}
+            </ul>
         </div>
     )
 }
--- src/components/comments.js
+++ src/components/comments.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-const Comments = ({userName,comment,removeComments},)=>{
+const Comments = ({comment,removeComments},)=>{
 
     const hour = comment.fullDate.hour;
     const minute = comment.fullDate.minute;
 
-    const declOfNum = (number, words) => {
-        
-        return words[(number % 100 > 4 && number % 100 < 20) ? 
-            2 : 
-                [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
-    }
-    
     return(
         <div key ={comment.id} className ="item-comment">
             <div className="flex-row">
@@ -19,7 +12,7 @@ const Comments = ({userName,comment,removeComments},)=>{
                 <div className="userName">
                     <span className="userName_span">Имя:</span>
                     {comment.name}
-                    <div>Время написания: {hour + declOfNum(hour, [' час ', ' часа ', ' часов ']) + minute + declOfNum(minute, [' минута ', ' минуты ', ' минут '])}</div>
+                    <div>Время написания: {hour}: {minute} </div>
                 </div>
 
                 <p className="comment-text">
